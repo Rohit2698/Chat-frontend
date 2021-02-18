79ee42a06623c79354de9714af4caff18a5e1368
Rohit Thakur <rohit.thakur2698@gmail.com>
2021-02-18 12:41:57 +0530

    Added frontend.

--- src/App.tsx
+++ src/App.tsx
@@ -1,25 +1,30 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import { Stack } from "@fluentui/react";
+import { Button, Container, TextField } from "@material-ui/core";
+import "./App.scss";
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <Container
+      fixed
+      style={{ height: "100vh", display: "flex", justifyContent: "center" }}
+    >
+      <Stack style={{ height: "100%", width: "50%" }}>
+        <Stack horizontalAlign="center" style={{ height: "10%" }}>
+          <h1>Chat Application</h1>
+        </Stack>
+        <Stack style={{ height: "70%" }}>
+          <h1>Chats</h1>
+        </Stack>
+        <Stack horizontal style={{ height: "10%" }}>
+          <form noValidate style={{ width: "100%" }} className={"message-form"}>
+            <TextField id="standard-basic" label="Write Message" />
+          </form>
+          <Button variant="contained" color="primary" style={{ height: "50%" }}>
+            Primary
+          </Button>
+        </Stack>
+      </Stack>
+    </Container>
   );
 }
 
